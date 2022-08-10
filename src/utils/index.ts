e1ce1063a7c57607eb56b31e249bb1d0820a62fb
Alex Metelli <alex-metelli@gmx.com>
2022-08-10 09:58:02 +0100

    refactor: renamed for clarity

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,15 +3,21 @@ import { uint256 } from 'starknet'
 
 export const dataToNumber = (value: any) => new BigNumber(value).toNumber()
 
+export const E18ToNumber = (value: any) => Math.floor(new BigNumber(value).toNumber() / 10 ** 18)
+
 export const bigDataToNumber = (value: any) => new BigNumber(uint256.uint256ToBN(value)).toNumber()
 
 export const calculEnoughResources = (
-  res: { metal: number; crystal: number; deuterium: number },
-  points: { metal: number; crystal: number; deuterium: number }
+  required: { metal: number; crystal: number; deuterium: number },
+  available: { metal: number; crystal: number; deuterium: number }
 ) => {
-  if (!points) {
+  if (!available) {
     return false
   }
 
-  return points.metal - res.metal >= 0 && points.crystal - res.crystal >= 0 && points.deuterium - res.deuterium >= 0
+  return (
+    available.metal - required.metal >= 0 &&
+    available.crystal - required.crystal >= 0 &&
+    available.deuterium - required.deuterium >= 0
+  )
 }
